a91ee0e8524bfbf80c2c5ab0efa0d2d24cdc4744
KangbingZhao <447953883@qq.com>
2022-12-17 20:54:24 +0800

    add modules

---
+++ rpc-framework-simple/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+group = "cn.zhaokangbing"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
 rootProject.name = "zero-to-rpc"
-
+include("example-client")
+include("example-server")
+include("rpc-framework-sample")
+include("rpc-framework-common")
+include("hello-service-api")
